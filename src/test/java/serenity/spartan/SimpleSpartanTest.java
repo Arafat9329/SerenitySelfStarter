da405b8069dcaadc4bdb97c7b7d4ef94b9616e35
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2020-12-26 12:33:04 -0500

    update

--- src/test/java/serenity/spartan/SimpleSpartanTest.java
+++ src/test/java/serenity/spartan/SimpleSpartanTest.java
@@ -1,32 +1,35 @@
 package serenity.spartan;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import net.serenitybdd.junit5.SerenityTest;
-import net.serenitybdd.rest.SerenityRest;
-import org.junit.jupiter.api.*;
-import static io.restassured.RestAssured.* ;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import net.serenitybdd.rest.Ensure;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static net.serenitybdd.rest.SerenityRest.reset;
+import static net.serenitybdd.rest.SerenityRest.when;
+import static org.hamcrest.Matchers.is;
 
 @SerenityTest
 public class SimpleSpartanTest {
 
     @BeforeAll
     public static void setUp(){
-        baseURI = "http://18.234.102.94";
-        basePath="/api";
-        port=8000;
+        RestAssured.baseURI = "http://18.234.102.94";
+        RestAssured.basePath="/api";
+        RestAssured.port=8000;
     }
 
     @AfterAll
     public static void afterAll(){
-reset();
+        RestAssured.reset();
     }
 
     @DisplayName("Testing GET/api hello endpoint")
     @Test
     public void testingHelloEndPoint(){
-        SerenityRest.
         when()
                 .get("/hello").
         then()
@@ -35,5 +38,9 @@ reset();
                 .log().body()
                 .body(is("Hello from Sparta"))
                 ;
+
+        Ensure.that("Make sure endpoint works",
+                response -> response.statusCode(200)
+        );
     }
 }
