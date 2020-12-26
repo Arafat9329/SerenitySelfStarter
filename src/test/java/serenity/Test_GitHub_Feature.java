987394e85df0f990bb50d14b66a1f15ce81b5015
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2020-12-26 12:59:26 -0500

    update

--- src/test/java/serenity/Test_GitHub_Feature.java
+++ src/test/java/serenity/Test_GitHub_Feature.java
@@ -20,40 +20,42 @@ import static org.hamcrest.Matchers.is;
 @SerenityTest
 public class Test_GitHub_Feature {
 
-    @DisplayName("This is for GitHub Test1")
+    @DisplayName("Initial DEMO for GitHub User test")
     @Test
-    @Tag("tag1")
+//    @Tag("tag1")
     public void testGitHubGetOneUserEndpointTest() {
 
         given()
                 .contentType(ContentType.JSON)
                 .pathParam("username","CybertekSchool").
-        when()
+                when()
                 .get("https://api.github.com/users/{username}").
-        then()
+                then()
+                // this is regular RestAssured way
                 .assertThat()
                 .statusCode(200)
-        //.log().all();
-        ;
-        Ensure.that("Test My GitHub Return the data1", response -> response.body("login", is("CybertekSchool")));
-
+                .body("login", is("CybertekSchool")) ;
+        // this is regular RestAssured way
 
     }
 
-    @DisplayName("DisplayName This is for GitHub Test2")
+    @DisplayName("This is serenity way of providing details")
     @Test
     public void testManyGitHubGetOneUserEndpoint2() {
 
         given()
-                .contentType(ContentType.JSON).
-        when()
-                .get("https://api.github.com/users/CybertekSchool").
-        then()
-                .assertThat()
-                .statusCode(200)
-        //.log().all();
-        ;
-        Ensure.that("This is supposed fail", response -> response.body("login", is("voila")));
+                .contentType(ContentType.JSON)
+                .pathParam("username","CybertekSchool").
+                when()
+                .get("https://api.github.com/users/{username}");
+
+
+        // this is serenity way we can generate custom report for validation steps
+        Ensure.that("Response was successful (instead of saying 200 : )",
+                response -> response.statusCode( 200  )
+        ) ;
+        Ensure.that("This is custom message for verifying login field in response",
+                response -> response.body("login", is("CybertekSchool")));
 
     }
 
