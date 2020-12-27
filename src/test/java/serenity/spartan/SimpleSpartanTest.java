6c81f0ce80f279583abb19b670bf1abb04ca0ad9
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2020-12-27 11:17:28 -0500

    update

--- src/test/java/serenity/spartan/SimpleSpartanTest.java
+++ src/test/java/serenity/spartan/SimpleSpartanTest.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static net.serenitybdd.rest.SerenityRest.reset;
-import static net.serenitybdd.rest.SerenityRest.when;
+
+import static net.serenitybdd.rest.SerenityRest.*;
 import static org.hamcrest.Matchers.is;
 
 @SerenityTest
@@ -43,4 +43,25 @@ public class SimpleSpartanTest {
                 response -> response.statusCode(200)
         );
     }
+
+    @DisplayName("This is serenity way of providing details")
+    @Test
+    public void testManyGitHubGetOneUserEndpoint2() {
+
+        given()
+                .contentType(ContentType.JSON)
+                .pathParam("username","CybertekSchool").
+                when()
+                .get("https://api.github.com/users/{username}");
+
+
+        // this is serenity way we can generate custom report for validation steps
+        Ensure.that("Response was successful (instead of saying 200 : )",
+                response -> response.statusCode( 200  )
+        ) ;
+        Ensure.that("This is custom message for verifying login field in response",
+                response -> response.body("login", is("CybertekSchool")));
+
+    }
+
 }
