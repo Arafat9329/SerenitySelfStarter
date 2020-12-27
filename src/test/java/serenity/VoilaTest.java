1fd9bba3fd60d9baa6ed5bb940d205f8eccaba57
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2020-12-27 15:10:47 -0500

    update

--- src/test/java/serenity/VoilaTest.java
+++ src/test/java/serenity/VoilaTest.java
@@ -1,13 +1,27 @@
 package serenity;
 
 import net.serenitybdd.junit5.SerenityTest;
+import net.thucydides.core.annotations.Steps;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import serenity.steps.B20Actions;
 
 @SerenityTest
+@Tag("voila")
 public class VoilaTest {
+
+    @Steps
+    B20Actions b20user;
 
    @Test
     public void testVolia(){
+
+       //GIVEN
+       b20user.expectSomeResult();
+       //WHEN
+       b20user.preparedSomething();
+       //THEN
+       b20user.takkeAnAction();
 
 
     }
--- src/test/java/serenity/steps/B20Actions.java
+++ src/test/java/serenity/steps/B20Actions.java
@@ -3,16 +3,17 @@ package serenity.steps;
 import net.thucydides.core.annotations.Step;
 
 public class B20Actions {
+    String actor;
 
-    @Step
+    @Step("#actor this is Step 1 name")
     public void preparedSomething(){
         System.out.println("preparing cool stuff");
     }
-    @Step
+    @Step("this is step 2 name looks you can name anuthings")
     public void  takkeAnAction(){
         System.out.println("taking some action");
     }
-    @Step
+    @Step("here is step 3 name ")
     public void expectSomeResult(){
         System.out.println("expecting some result");
     }
